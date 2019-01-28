4956bd6db503a0f62ed33a995df7331504023028
Lars3n95 <18326664+Lars3n95@users.noreply.github.com>
2019-01-28 21:26:24 +0100

    Updates

---
+++ precache-manifest.6b61c021106e100e2c097ab0811926ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6644615e4fc1715ffea2bec8b3abcbd7",
+    "url": "/static/media/zahlenspiel.6644615e.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8210e93711e4abfcd3ed",
+    "url": "/static/js/main.8210e937.chunk.js"
+  },
+  {
+    "revision": "7a9ea37d99132ae94266",
+    "url": "/static/js/1.7a9ea37d.chunk.js"
+  },
+  {
+    "revision": "8210e93711e4abfcd3ed",
+    "url": "/static/css/main.6e9a3ece.chunk.css"
+  },
+  {
+    "revision": "3f7208b4ae955aacadf33a1013861f7b",
+    "url": "/index.html"
+  }
+];
