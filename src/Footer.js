41cb75caf029bfda878d33c9cf88af626ebfbd0a
Lars3n95 <18326664+Lars3n95@users.noreply.github.com>
2021-09-21 21:08:42 +0200

    Update Privacy Policy

--- src/Footer.js
+++ src/Footer.js
@@ -12,7 +12,7 @@ let strings = new LocalizedStrings({
         abouttext: "The following information (Impressum) is required under German law.",
         purchasesheader: "",
         purchasetext: "",
-        privacypolicyheader: "Privacy Policy (from 01.03.2021)",
+        privacypolicyheader: "Privacy Policy (from 21.09.2021)",
         privacypolicy: "\
 <div>This Privacy Policy informs the user about what data is collected, why it is collected, and what is done with it.</div><br/> \
 <b>About</b><br/> \
@@ -23,29 +23,40 @@ We draw your attention to the fact that data transmission over the Internet (e.g
 In the apps itself, no personal data is collected through a download/purchase/in-app purchase. \
 However, this data may be collected and processed by the provider of the trading platform and the responsible payment processor. \
 Please refer to the corresponding data protection declaration below:<br/> \
-Android: https://policies.google.com/privacy <br/> \
-iOS: https://www.apple.com/legal/privacy/en-ww/ <br/> \
+Android: <a href=\"https://policies.google.com/privacy\">https://policies.google.com/privacy</a> <br/> \
+iOS: <a href=\"https://www.apple.com/legal/privacy/en-ww/\">https://www.apple.com/legal/privacy/en-ww/</a> <br/> \
 We have no control over the collection of personal information from the above parties.<br/><br/> \
  \
 <b>App</b><br/> \
 You can use the apps without providing any personal data. The apps itself do not collect personal data at any time.<br/> \
 Android: <br/>\
 The app offers the possibility to connect with the Google Play Games Services. By doing this it might be that personal data is collected by the services.<br/> \
-The app is using Firebase. A software which helps to improve the app by sending crash reports. The crash reports do not collect personal data.<br/> \
-You can find the corresponding privacy policy of both services at: https://policies.google.com/privacy<br/> \
+You can find the corresponding privacy policy at: <a href=\"https://policies.google.com/privacy\">https://policies.google.com/privacy</a><br/> \
+The only data Numbers Game 2 collects and sends to Amazon Web Services (AWS) is the information what level you solved and how many coins you had in that moment. \
+That data is used to better balance the game and does not contain any personal information. \
 By using the Apps you agree to this privacy policy. If you do not agree to this privacy policy please do not use the Apps<br/><br/> \
- \
+\
+<b>Google AdMob</b><br/> \
+<div>In some apps it is possible to watch ads in order to receive in-game rewards. For this we use Google AdMob.<br/> \
+Google AdMob is an online advertising program from Google LLC., 1600 Amphitheatre Parkway, Mountain View, CA 94043.<br/> \
+Before you decide to watch the ads, you must agree once that you are at least 18 years old and that personal data may be collected and processed by Google LLC. for this purpose. \
+For this purpose, Google stores cookies or other identifiers and collects and processes, among other things, data about the use of the app and the device, the IP address, the device ID and data about previous ad interactions. \
+If you have deactivated personalized ads in the settings of the respective app, Google LLC. only stores cookies or other identifiers for capping the advertising frequency (frequency capping), aggregated ad reports and for combating fraud and abuse.<br/> \
+In California, the European Economic Area, and the United Kingdom, only non-personalized ads are shown, regardless of the setting, and thereby only cookies or other identifiers are used for the purposes just mentioned. \
+You can also generally disable personalized ads in your device settings. To do so, click on \"Google\" -> \"Ads\" -> \"Opt out of Ads Personalization\" in the settings.<br/> \
+More details on the use of personal data by Google LLC. can be found in the privacy policy at <a href=\"https://policies.google.com/privacy\">https://policies.google.com/privacy</a> and specifically for the area of advertising at <a href=\"https://policies.google.com/technologies/ads\">https://policies.google.com/technologies/ads</a>.</div><br/> \
+\
 <b>UserReport</b><br/> \
 Some apps and the website use UserReport to collect feedback and feature/bug reports from users. You do not have to use this feature as you can always can contact me via e-mail.<br/> \
 The only information I get from UserReport is the feature/bug report, your name (you can choose any name you want) and your e-mail address.<br/> \
-UserReport might collect further data. You can find the privacy policy at: https://privacy.userreport.com/en-US/for-users/privacy-policy/<br/><br/> \
+UserReport might collect further data. You can find the privacy policy at: <a href=\"https://privacy.userreport.com/en-US/for-users/privacy-policy\">https://privacy.userreport.com/en-US/for-users/privacy-policy</a><br/><br/> \
 \
 <b>Website</b><br/> \
-The website https://lars3n95.github.io/ can be used without providing any personal data. \
+The website <a href=\"https://lars3n95.github.io/\">https://lars3n95.github.io</a> can be used without providing any personal data. \
 The website itself does not collect personal data at any time.<br/> \
 The website is hosted at GitHub. By doing this it might be that personal data is collected by the service.<br/> \
-You can find the corresponding privacy policy of the service at: https://help.github.com/en/articles/github-privacy-statement<br/> \
-In addition a random unique id is generated from https://www.freevisitorcounters.com and saved as cookie to be able to see how many users visit the webiste. \
+You can find the corresponding privacy policy of the service at: <a href=\"https://help.github.com/en/articles/github-privacy-statement\">https://help.github.com/en/articles/github-privacy-statement</a><br/> \
+In addition a random unique id is generated from <a href=\"https://www.freevisitorcounters.com\">https://www.freevisitorcounters.com</a> and saved as cookie to be able to see how many users visit the webiste. \
 The cookie/id will be deleted when the browser is closed. \
 <br/> \
 By using the website you agree to this privacy policy. If you do not agree to this privacy policy please do not use the website<br/><br/> \
@@ -78,7 +89,7 @@ The Privacy Policy may change from time to time during the futures services upda
         abouttext: "Angaben gemäß §5 TMG",
         purchasesheader: "Informationen zu (In-)App-Verkäufen",
         purchasetext: "Gemäß § 19 UStG wird vom Anbieter keine Umsatzsteuer berechnet. Der Händler (App Store) kann jedoch unabhängig davon Umsatzsteuer berechnen und abführen.",
-        privacypolicyheader: "Datenschutzerklärung (Stand 01.03.2021)",
+        privacypolicyheader: "Datenschutzerklärung (Stand 21.09.2021)",
         privacypolicy: "\
 <b>Geltungsbereich</b> \
 <div>Diese Datenschutzerklärung klärt Nutzer über die Art, den Umfang und Zwecke der Erhebung und Verwendung personenbezogener Daten in von dem Anbieter (Lars Feßen-Fallsehr) entwickelten Apps und Webseiten auf.</div><br/>\
@@ -96,8 +107,8 @@ Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglic
 <div>In den Apps selbst werden keine personenbezogenen Daten durch einen Download/Kauf/In-App Kauf erhoben.  \
 Diese Daten werden jedoch möglicherweise vom Anbieter der Handelsplattform und dem zuständigen Zahlungsabwickler erhoben und verarbeitet. \
 Bitte beachten Sie die entsprechende Datenschutzerklärung unter: <br/> \
-Android: https://policies.google.com/privacy <br/> \
-iOS: https://www.apple.com/legal/privacy/de-ww/ <br/> \
+Android: <a href=\"https://policies.google.com/privacy\">https://policies.google.com/privacy</a> <br/> \
+iOS: <a href=\"https://www.apple.com/legal/privacy/de-ww/\">https://www.apple.com/legal/privacy/de-ww/</a> <br/> \
 Wir haben keinen Einfluss auf die Erhebung personenbezogener Daten der oben genannten Parteien.</div> \
  \
 <br/> \
@@ -105,25 +116,37 @@ Wir haben keinen Einfluss auf die Erhebung personenbezogener Daten der oben gena
 <div>Die Nutzung der Apps ist ohne Angabe personenbezogener Daten möglich. Von dem Anbieter selbst werden während der Nutzung keine personenbezogene Daten erhoben. <br/> \
 Android: <br/>\
 Die App bietet die Möglichkeit sich mit den Google Play Spiele Services zu verbinden. Dabei werden von diesen Services möglicherweise personenbezogene Daten erhoben. \
-Um die App zu verbessern wird Firebase verwendet, womit Absturzberichte an den Entwickler gesendet werden. Diese enthalten keine personenbezogenen Daten. \
-Die Datenschutzerklärung beider Services erhalten Sie unter: https://policies.google.com/privacy \
+Die Datenschutzerklärung erhalten Sie unter: <a href=\"https://policies.google.com/privacy\">https://policies.google.com/privacy</a> \
+Die einzigen Daten, die in der App Zahlenspiel 2 gesammelt und an Amazon Web Services (AWS) gesendet werden, ist die Information, welches Level Sie gelöst haben und wie viele Münzen Sie in dem Moment besaßen. \
+Diese Informationen werden dafür benutzt, das Spiel zu optimieren und beinhalten keinerlei personenbezogene Daten. \
 <br/> \
 Durch die Nutzung dieser Apps und ihrer Dienste bringen Sie Ihr Einverständnis zu dieser Richtlinie und der Verarbeitung Ihrer Daten gemäß dieser Richtlinie, insbesondere Ihrer personenbezogenen Daten, zum Ausdruck.  \
 Bitte nutzen Sie die Apps und ihre Dienste nicht, wenn Sie mit diesen Bestimmungen nicht einverstanden sind.</div> \
- \
+\
+<br/> \
+<b>Google AdMob</b> \
+<div>In manchen Apps ist es möglich, Werbung zu schauen, um dafür eine Ingame-Belohnung zu erhalten. Dafür nutzen wir Google AdMob.<br/> \
+Google AdMob ist ein Online-Werbeprogramm von Google LLC., 1600 Amphitheatre Parkway, Mountain View, CA 94043.<br/> \
+Bevor Sie die Werbung anschauen, müssen Sie einmalig zustimmen, dass dafür von Google LLC. ggf. personenbezogene Daten erfasst und verarbeitet werden. \
+Dafür speichert Google Cookies oder andere Kennungen und sammelt und verarbeitet unter anderem Daten über die Verwendung der App und des Gerätes, die IP-Adresse, die Geräte-ID und Daten zu vorherigen Anzeige-Interaktionen. \
+Haben Sie in den Einstellungen der jeweiligen App die personalisierte Werbung deaktiviert, speichert Google LLC. ausschließlich Cookies oder andere Kennungen für die Deckelung der Werbe-Frequenz (Frequency Capping), aggregierte Anzeigen-Reports und zur Bekämpfung von Betrug und Missbrauch.<br/> \
+In Kalifornien, dem Europäischen Wirtschaftsraum und dem Vereinigten Königreich wird ausschließlich, unabhängig von der Einstellung, nicht personalisierte Werbung gezeigt und dadurch ausschließlich für die soeben genannten Zwecke Cookies oder andere Kennungen verwendet. \
+Sie können in Ihren Geräte-Einstellungen auch generell die personalisierte Werbung deaktivieren. Klicken Sie dazu in den Einstellungen auf \"Google\" -> \"Anzeigen\" -> \"Personalisierte Werbung deaktivieren\".<br/> \
+Genauere Details zur Verwendung personenbezogener Daten von Google LLC. finden Sie in der Datenschutzerklärung unter <a href=\"https://policies.google.com/privacy\">https://policies.google.com/privacy</a> und konkret für den Bereich Werbung unter <a href=\"https://policies.google.com/technologies/ads\">https://policies.google.com/technologies/ads</a>. </div> \
+\
 <br/> \
 <b>UserReport</b> \
 <div>Manche Apps und die Webseite nutzen UserReport, um Featurewünsche oder Fehlermeldungen zu sammeln. Sie müssen UserReport nicht nutzen, sondern können uns immer auch per E-Mail kontaktieren. \
 Die einzigen Informationen, die wir von UserReport erhalten, sind der Featurewunsch/die Fehlermeldung, Ihr eingegebener Name und Ihre E-Mail Adresse. \
-UserReport sammelt zusätzlich möglicherweise noch weitere Daten. Die Datenschutzerklärung von UserReport finden Sie hier: https://privacy.userreport.com/de-DE/for-users/privacy-policy/ </div> \
+UserReport sammelt zusätzlich möglicherweise noch weitere Daten. Die Datenschutzerklärung von UserReport finden Sie hier: <a href=\"https://privacy.userreport.com/de-DE/for-users/privacy-policy\">https://privacy.userreport.com/de-DE/for-users/privacy-policy</a> </div> \
 \
 <br/> \
 <b>Webseite</b> \
-<div>Die Nutzung der Webseite https://lars3n95.github.io/ ist ohne Angabe personenbezogener Daten möglich. Von dem Anbieter selbst werden während der Nutzung keine personenbezogene Daten erhoben. \
+<div>Die Nutzung der Webseite <a href=\"https://lars3n95.github.io/\">https://lars3n95.github.io</a> ist ohne Angabe personenbezogener Daten möglich. Von dem Anbieter selbst werden während der Nutzung keine personenbezogene Daten erhoben. \
 Die Webseite wird bei GitHub gehostet. Dabei werden von diesem Service möglicherweise personenbezogene Daten erhoben. \
-Die Datenschutzerklärung des Services erhalten Sie unter: https://help.github.com/en/articles/github-privacy-statement \
+Die Datenschutzerklärung des Services erhalten Sie unter: <a href=\"https://help.github.com/en/articles/github-privacy-statement\">https://help.github.com/en/articles/github-privacy-statement</a> \
 <br/> \
-Zusätzlich wird eine eindeutige zufällige ID von https://www.freevisitorcounters.com als Cookie gespeichert, um einsehen zu können, wie viele Nutzer die Webseite besuchen. \
+Zusätzlich wird eine eindeutige zufällige ID von <a href=\"https://www.freevisitorcounters.com\">https://www.freevisitorcounters.com</a> als Cookie gespeichert, um einsehen zu können, wie viele Nutzer die Webseite besuchen. \
 Die ID wird spätestens gelöscht, sobald Sie den Browser verlassen. \
 <br/> \
 Durch die Nutzung der Webseite bringen Sie Ihr Einverständnis zu dieser Richtlinie und der Verarbeitung Ihrer Daten gemäß dieser Richtlinie, insbesondere Ihrer personenbezogenen Daten, zum Ausdruck.  \
@@ -159,7 +182,7 @@ Von uns gespeicherte Daten werden, sollten sie für ihren Verwendungszweck nicht
  \
 <br/> \
 <br/> \
-<div>Stand 01.03.2021</div> \
+<div>Stand 21.09.2021</div> \
 <div>Die Datenschutzerklärung kann sich von Zeit zu Zeit ändern. Sie wird hier auf der Webseite aktuell gehalten.</div> "
     }
 });
@@ -175,6 +198,15 @@ class Footer extends Component {
         }
         return (
             <div className={classNames}>
+                <Modal open={window.location.hash === "#pp"}>
+                    <Modal.Header>{strings.privacypolicyheader}</Modal.Header>
+                    <Modal.Content>
+                        <Modal.Description>
+                            <Image src={require('./resources/impressum.png')} />
+                            <div dangerouslySetInnerHTML={{ __html: strings.privacypolicy }}></div>
+                        </Modal.Description>
+                    </Modal.Content >
+                </Modal>
                 <Grid columns='equal' stackable divided>
                     <Grid.Column>
                         <Modal trigger={<h4 style={{ textAlign: 'center', cursor: 'pointer' }}>{strings.about}</h4>}>
